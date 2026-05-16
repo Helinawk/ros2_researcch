4e37d7d02d5556df79243b73c59438ef5e4f2028
Helinawk <ivan.biryukov.2002@mail.ru>
2026-05-16 22:59:27 +0400

    add WS

---
+++ workspace/build/latency_test/build/lib/latency_test/subscriber_node.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import time
+
+
+class SubscriberNode(Node):
+    def __init__(self):
+        super().__init__('subscriber_node')
+
+        self.subscription = self.create_subscription(
+            String,
+            'latency_topic',
+            self.callback,
+            10
+        )
+    def callback(self, msg):
+        # Разбираем строку: номер сообщения и время отправки
+        message_id_str, sent_time_str = msg.data.split(',')
+
+        message_id = int(message_id_str)
+        sent_time = int(sent_time_str)
+        receive_time = time.time_ns()
+
+        latency_ms = (receive_time - sent_time) / 1_000_000
+
+        # Логируем в CSV (append). Файл будет в корне workspace.
+        with open('/root/ros2_ws/latency_results.csv', 'a') as f:
+            f.write(f'{message_id},{sent_time},{receive_time},{latency_ms}\n')
+
+        # Выводим задержку в консоль для контроля
+        self.get_logger().info(f'ID: {message_id}, Latency: {latency_ms:.3f} ms')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SubscriberNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
---
+++ workspace/install/latency_test/lib/python3.10/site-packages/latency_test/publisher_node.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import time
+
+
+class PublisherNode(Node):
+    def __init__(self):
+        super().__init__('publisher_node')
+
+        self.publisher = self.create_publisher(
+            String,
+            'latency_topic',
+            10
+        )
+
+        self.timer = self.create_timer(0.01, self.publish_message)
+        self.counter = 0
+
+    def publish_message(self):
+        msg = String()
+        msg.data = f"{self.counter},{time.time_ns()}"
+        self.publisher.publish(msg)
+        
+        self.counter += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PublisherNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
